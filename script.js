1c2dd78fe25dc94ffff050341aa044dc5dc20c9d
jean7111 <jeanmasurel0@gmail.com>
2021-04-20 19:02:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+var conso_base = 6.3;
 
 function CalculConso() {
     var NbKm = document.getElementById("input1").value;
@@ -10,14 +11,13 @@ function CalculConso() {
 
         var augmentation = ((Number(resultConso) - conso_base)/ conso_base) * 100;
         augmentation = String(Math.round(augmentation*100)/100);
-        console.log(augmentation)
         document.getElementById("reponse").innerHTML = Math.round(resultConso*100)/100 + " L/100 km";
         document.getElementById("aug").innerHTML = Math.round(augmentation*100)/100 + " %";  
     }
 
 }
 
-function change_conso_base() {
-    var conso_base = document.getElementById("input-conso-base").value;
+// function change_conso_base() {
+//     var conso_base = document.getElementById("input-conso-base").value;
 
-}
+// }
